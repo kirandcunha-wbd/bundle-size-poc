087a3839e06a28dd95271a5961ffea9e6d2c60a6
Kiran D Cunha <kiran_dcunha@discovery.com>
2023-04-10 17:29:42 +0530

    chore: added build player step to better demonstrate dynamic download

--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
 // Test import of styles
 import '@/styles/index.scss'
 
-let shaka;
+const app = document.querySelector('#root')
 
-if (!localStorage.count) {
-    localStorage.count = 1
+function buildPlayer() {
+    let shaka;
+
+    if (!localStorage.count) {
+        localStorage.count = 1
+    }
+
+
+    const version = ++localStorage.count % 3
+
+    switch (version) {
+        case 0:
+            import(`shaka-player-4.3.5`).then((e) => {
+                shaka = e
+                done()
+            });
+            break
+        case 1:
+            import(`shaka-player-2.5.10`).then((e) => {
+                shaka = e
+                done()
+            });
+            break
+        case 2:
+            import(`shaka-player-3.3.0`).then((e) => {
+                shaka = e
+                done()
+            });
+            break
+    }
+
+    function done() {
+        const heading = document.createElement('h1')
+        window.shaka = shaka
+        heading.textContent = "Shaka version " + shaka?.Player?.version
+        app.append(heading)
+    }
 }
 
-const version = ++localStorage.count % 3
 
-switch (version) {
-    case 0:
-        import(`shaka-player-4.3.5`).then((e) => {
-            shaka = e
-            done()
-        });
-        break
-    case 1:
-        import(`shaka-player-2.5.10`).then((e) => {
-            shaka = e
-            done()
-        });
-        break
-    case 2:
-        import(`shaka-player-3.3.0`).then((e) => {
-            shaka = e
-            done()
-        });
-        break
+const button = document.createElement('button')
+button.textContent = "Build Player"
+button.onclick = () => {
+    buildPlayer()
 }
-
-function done() {
-    const heading = document.createElement('h1')
-    window.shaka = shaka
-    heading.textContent = "Shaka version " + shaka?.Player?.version
-
-    const app = document.querySelector('#root')
-    app.append(heading)
-}
+app.append(button)
